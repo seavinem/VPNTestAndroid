02047f47fa9e48e643f60a1b41d19f1b187f2365
Kirill Kozlov <kirillkozlov692@gmail.com>
2026-04-07 01:45:04 +0300

    Fix featured server order

--- data/home/src/main/java/com/org/home/repository/HomeRepositoryImpl.kt
+++ data/home/src/main/java/com/org/home/repository/HomeRepositoryImpl.kt
@@ -5,15 +5,39 @@ import com.org.home.model.CountryDomain
 import com.org.network.service.VpnWebService
 import javax.inject.Inject
 
+private val featuredServerNames = listOf(
+    "Germany",
+    "United States",
+    "Japan",
+    "Netherlands"
+)
+
 class HomeRepositoryImpl @Inject constructor(
     private val vpnWebService: VpnWebService,
     private val countryDtoToDomainMapper: CountryDtoToDomainMapper
 ) : HomeRepository {
 
-    override suspend fun getAvailableCountries(): List<CountryDomain> = vpnWebService
-        .getAvailableCountries()
+    override suspend fun getAvailableCountries(): List<CountryDomain> {
+        val countriesByName = vpnWebService
+            .getAvailableCountries()
             .map { countryDtoToDomainMapper(it) }
             .filter { it.countryName.isNotBlank() }
             .distinctBy { it.countryName }
+            .associateBy { it.countryName }
+
+        val featuredCountries = featuredServerNames.map { featuredCountryName ->
+            countriesByName[featuredCountryName] ?: CountryDomain(
+                countryName = featuredCountryName,
+                capital = "",
+                flagUrl = ""
+            )
+        }
+
+        val featuredServerNamesSet = featuredServerNames.toSet()
+        val remainingCountries = countriesByName.values
+            .filterNot { country -> country.countryName in featuredServerNamesSet }
             .sortedBy { it.countryName.lowercase() }
+
+        return featuredCountries + remainingCountries
+    }
 }
