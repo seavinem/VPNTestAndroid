978ebbff23fa750e3ff25062aa92ffc7e0090427
Kirill Kozlov <kirillkozlov692@gmail.com>
2026-04-07 11:00:21 +0300

    Removed Random from VPN Mock

--- core/vpn/src/main/java/com/org/vpn/service/MockVpnService.kt
+++ core/vpn/src/main/java/com/org/vpn/service/MockVpnService.kt
@@ -1,22 +1,28 @@
 package com.org.vpn.service
 
 import com.org.vpn.model.VpnState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
-import kotlin.random.Random
 
 class MockVpnService @Inject constructor() {
 
     fun startConnection(country: String): Flow<VpnState> = flow {
         emit(VpnState.CONNECTING)
-        delay(2_000L)
+        delay(DELAY_TWO_SECONDS)
 
-        if (country.isBlank() || !Random.nextBoolean()) {
+        if (country.isBlank()) {
             throw IllegalStateException("Connection failed")
         }
 
         emit(VpnState.CONNECTED)
+    }.flowOn(Dispatchers.IO)
+
+    private companion object {
+
+        const val DELAY_TWO_SECONDS = 2_000L
     }
 }
